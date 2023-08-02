5c34f44d54af20c674f668a8d5534b09b27f02a0
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2023-08-02 20:38:40 +0200

    Change backend name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "tf-s3-backend"
+        bucket = "tf-backend-mj"
         key    = "development/terraform_state"
         region = "us-east-1"
     }
